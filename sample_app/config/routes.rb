17990d009fb823c46d6570f8ace9737244cceb8b
Sean Pypers <seapyp@gmail.com>
2016-07-25 16:44:09 -0700

    Adds User Profile page and flash message after initial sign in

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
-  get  '/users',   to: 'users#new'
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
